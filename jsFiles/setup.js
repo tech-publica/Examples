c524716fcfc07f8606cb5f3f297dbb4cc8c67e50
tech publica <tech.publica@gmail.com>
2025-05-19 10:32:53 +0200

    first commit

---
+++ jsFiles/setup.js
@@ -0,0 +1,26 @@
+let x = 3;
+console.log(x);
+console.log(x * x + 1);
+class Example {
+    name = "ciccio";
+}
+//
+export class ExampleModule {
+    #privateVariable;
+    constructor() {
+        this.#privateVariable = 'I am private';
+    }
+    privateMethod() {
+        return 'Private Method Output';
+    }
+    publicMethod() {
+        return `Public Method Output - ${this.#privateVariable}`;
+    }
+}
+let num = 5;
+let str = "5";
+// if (num == str) {
+//   console.log("Equal");
+// } else {
+//   console.log("Not Equal");
+// }
---
+++ tsFiles/setup.ts
@@ -0,0 +1,32 @@
+let x = 3;
+console.log(x);
+console.log(x * x + 1);
+class Example {
+  private name: string = "ciccio";
+}
+
+//
+export class ExampleModule {
+  #privateVariable: string;
+
+  constructor() {
+    this.#privateVariable = 'I am private';
+  }
+
+  private privateMethod(): string {
+    return 'Private Method Output';
+  }
+
+  publicMethod(): string {
+    return `Public Method Output - ${this.#privateVariable}`;
+  }
+}
+
+let num: number = 5;
+let str: string = "5";
+
+// if (num == str) {
+//   console.log("Equal");
+// } else {
+//   console.log("Not Equal");
+// }
